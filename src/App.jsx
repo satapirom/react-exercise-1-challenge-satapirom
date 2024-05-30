4a10b262171286a1a11324a17ad5b89a46fa72c9
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-30 21:01:15 +0700

    Routes test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import FunctionComponent from './components/1.1_myFirstReactComponent/01_FunctionComponent'
 import FunctionalComponent from './components/1.1_myFirstReactComponent/02_FunctionalComponent';
 import ClassComponent from './components/1.1_myFirstReactComponent/03_ClassComponent';
 
 function App() {
   return (
-    <div>
-      <FunctionComponent/>
-      <FunctionalComponent/>
-      <ClassComponent/>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div>
+          <FunctionComponent />
+          <FunctionalComponent />
+          <ClassComponent />
+        </div>} />
+        <Route path="page01" element={<FunctionComponent />} />
+        <Route path="page02" element={<FunctionalComponent />} />
+        <Route path="page03" element={<ClassComponent />} />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
